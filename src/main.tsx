24c289206f22004e42ecea9d25b15d8df8afe55e
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-09-29 21:20:37 -0600

    refactor: comment out GifsApp component in main render

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { GifsApp } from './GifsApp'
+// import { GifsApp } from './GifsApp'
+import { MyCounterApp } from './counter/components/MyCounterApp'
 
 import './index.css'
 
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <GifsApp />
+        {/* <GifsApp /> */}
+        <MyCounterApp />
     </StrictMode>,
 )
