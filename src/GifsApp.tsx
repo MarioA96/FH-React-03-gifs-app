08aa8c4ef75a318795992c02b5b8c9bb5215c4bf
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-09-29 21:20:29 -0600

    refactor: reorganize imports and update state management for GIFs and previous search terms

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,41 +1,33 @@
-import { mockGifs } from "./mock-data/gifs.mock"
+import { useState } from "react"
+
+import { GifList } from "./gifs/components/GifList"
+import { PreviousSearches } from "./gifs/components/PreviousSearches"
 
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { SearchBar } from "./shared/components/SearchBar"
-import { PreviousSearches } from "./gifs/components/PreviousSearches"
-import { GifList } from "./gifs/components/GifList"
-import { useState } from "react"
+
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
+import type { Gif }  from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
 
-    const [previousTerms, setPreviousTerms] = useState(['dragon ball z','saitama']);
+    const [gifs, setGifs] = useState<Gif[]>([]);
+    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
     const handleTermClicked = (term: string) => {
         console.log(term);
     }
 
-    const handleSearch = (query: string = "") => {
-
+    const handleSearch = async(query: string = "") => {
         query = query.trim().toLowerCase();
         
         if(query.length === 0) return ;
-
         if(previousTerms.includes(query)) return ;
 
         setPreviousTerms( [query, ...previousTerms].splice(0,7) );
-        // if(query !== ""){
-        //     if(query in previousTerms) return ;
 
-        //     if(previousTerms.length < 8){
-        //         query = query.toUpperCase();
-        //         query = query.trim();
-        //         setPreviousTerms([query, ...previousTerms]);
-        //     } else {
-        //         return ;
-        //     }
-        // } else {
-        //     return ;
-        // }
+        const gifs = await getGifsByQuery(query);
+        setGifs(gifs);
     }
 
     return (
@@ -53,7 +45,7 @@ export const GifsApp = () => {
             <PreviousSearches searches={previousTerms} onLabelClicked={ handleTermClicked }/>
 
             {/* Gifs */}
-            <GifList gifs={mockGifs} />
+            <GifList gifs={gifs} />
         </>
     )
 }
