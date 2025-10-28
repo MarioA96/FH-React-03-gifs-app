84d39949e706a135fa1cfb9ab10fa6d013945ab1
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-27 21:26:22 -0600

    fix: add validation to return empty list for empty query in getGifsByQuery function

--- src/gifs/actions/get-gifs-by-query.action.ts
+++ src/gifs/actions/get-gifs-by-query.action.ts
@@ -5,26 +5,37 @@ import type { Gif } from '../interfaces/gif.interface'
 
 export const getGifsByQuery = async(query: string): Promise<Gif[]> => {
     
-    const response = await giphyApi<GiphyResponse>(
-        '/search',
-        {
-            params: {
-                q: query,
-                limit: 10,
-                // Below from giphyApi
-                // lang: 'es',
-                // api_key: import.meta.env.VITE_GIPHY_API_KEY,
-                // api_key: 'oYQa7B95z3geWHQdq87i2AO4IKEHMiky'
-            }
-        }
-    );
+    if(query.trim().length === 0){
+        return [];
+    }
 
-    return response.data.data.map( (gif)=>({
-        id: gif.id,
-        title: gif.title,
-        url: gif.images.original.url,
-        width: Number(gif.images.original.width),
-        height: Number(gif.images.original.height),
-    }) );
+    try {
+        const response = await giphyApi<GiphyResponse>(
+            '/search',
+            {
+                params: {
+                    q: query,
+                    limit: 10,
+                    // Below from giphyApi
+                    // lang: 'es',
+                    // api_key: import.meta.env.VITE_GIPHY_API_KEY,
+                    // api_key: 'oYQa7B95z3geWHQdq87i2AO4IKEHMiky'
+                }
+            }
+        );
+    
+        return response.data.data.map( (gif)=>({
+            id: gif.id,
+            title: gif.title,
+            url: gif.images.original.url,
+            width: Number(gif.images.original.width),
+            height: Number(gif.images.original.height),
+        }) );
+
+    } catch(error) {
+        console.error(error);
+        return [];
+    }
+
 
 }
